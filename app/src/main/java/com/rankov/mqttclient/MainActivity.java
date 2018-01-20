5958ddc1fd0abb106ab50bc15709b1caf897b708
Andrey Rankov <andreyr@appcard.com>
2018-01-20 06:20:33 +0200

    Initial commit

---
+++ app/src/main/java/com/rankov/mqttclient/MainActivity.java
@@ -0,0 +1,101 @@
+package com.rankov.mqttclient;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ActiveMQ";
+    public static final String clientId = "any_client_name";
+    public static final String serverURI = "tcp://192.168.1.15:1883";
+    public static final String publishTopic = "outbox";
+    public static final String subscribeTopic = "inbox";
+
+
+    MqttAndroidClient client;
+
+    private Button publishBtn;
+    private EditText messageEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        connect();
+    }
+
+    private void initViews() {
+        publishBtn = (Button) findViewById(R.id.publish);
+        messageEt = (EditText) findViewById(R.id.ntrMessage);
+        publishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishMessage(messageEt.getText().toString());
+            }
+        });
+        publishBtn.setEnabled(false);
+    }
+
+    private void connect() {
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setAutomaticReconnect(true);
+
+        client = new MqttAndroidClient(this, serverURI, clientId);
+        try {
+            client.connect(connectOptions, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    subscribe();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void subscribe() {
+        try {
+            client.subscribe(subscribeTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(final String topic, final MqttMessage message) throws Exception {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, message.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void publishMessage(String message) {
+        MqttMessage msg = new MqttMessage();
+        msg.setPayload(message.getBytes());
+        try {
+            client.publish(publishTopic, msg);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
